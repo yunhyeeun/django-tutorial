de835d82cccb156edfd040a4ddc17dc286edf55f
yunhyeeun <dmsdbsgP0320@gmail.com>
2021-01-07 13:30:37 +0900

    Setup mysql database

--- mytutorial/settings.py
+++ mytutorial/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'mytutorial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
