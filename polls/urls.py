d33ddae9adf51e78ce32d5219652c523b7e6ce7d
yunhyeeun <dmsdbsgP0320@gmail.com>
2021-01-07 16:50:05 +0900

    Create the template system

--- polls/urls.py
+++ polls/urls.py
@@ -16,6 +16,10 @@ path (ROUTE, VIEW, KWARGS, NAME)
 
 '''
 
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
