1aacc08b39ffd3f6e9746a23049e81a9b73780fd
yunhyeeun <dmsdbsgP0320@gmail.com>
2021-01-22 13:00:03 +0900

    Modify database info

--- coronareport/coronareport/settings.py
+++ coronareport/coronareport/settings.py
@@ -78,8 +78,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'coronareport',
-        'USER': 'root',
-        'PASSWORD': '',
+        'USER': 'test',
+        'PASSWORD': 'coronareport',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
